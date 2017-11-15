9c0b1942973a6e59c316928403da517ca1fe225e
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2017-11-15 16:32:02 +0100

    added a stub of the project script

---
+++ project_script.py
@@ -0,0 +1,61 @@
+import requests, urllib, string, json
+import pymongo
+from pymongo import *
+
+# returns a list of results
+# documentation: https://dandelion.eu/docs/api/datatxt/nex/v1/#response
+def dandelion_ner(text):
+ #print "Dandelion"
+ url = "https://api.dandelion.eu/datatxt/nex/v1"
+ headers = {'text':text,'lang':'it','include':'types,lod,categories','epsilon':'0.3', 'token': 'f3238f9b8e974df09b6814de9e9de532'}
+ r = requests.get(url,params=headers)
+ if r.status_code == requests.codes.ok:
+  data = json.loads(r.text)
+ if "error" in data.keys():
+  return "API error. Status: "+str(data['status'])+" Code: "+data['code']+" Message: "+data['message']
+ results = list()
+ # process Dandelion (Spazio dati) data
+ # all entity types are dbpedia.org/ontology entities
+ for item in data['annotations']:
+  entity_type = ", ".join([w.split("/")[-1] for w in item['types']])
+  entity_category = ""
+ if "categories" in item.keys():
+  entity_category = ", ".join([w.split("/")[-1] for w in item['categories']])
+  dbpedia = ""
+  wikipedia = ""
+ if "lod" in item.keys():
+  dbpedia = item['lod']['dbpedia']
+  wikipedia = item['lod']['wikipedia']
+  results.append({"type": entity_type, "category": entity_category, "relevance": item['confidence'], "word": item['spot'], "offset": item['start'], "identifier": item['uri'], "title": item['title'], "dbpedia": dbpedia, "wikipedia": wikipedia})
+ #print "id "+urllib.unquote(item['uri']).encode('latin-1')
+  return results
+ else:
+  return "Web error: "+str(r.status_code)
+  
+def main():
+    client = MongoClient('128.178.60.49', 27017)
+    client.linkedbooks_dev.authenticate('lb_pulse', '1243')
+    db = client.linkedbooks_dev
+    book_metadata = db.metadata.find({"type_document": "monograph"}, limit=1)
+    for metadata in book_metadata:
+        print(metadata)
+        bid = metadata["bid"]
+        book = db.documents.find_one({"bid": bid})
+        pages = book["pages"]
+        fulltext = ""
+        for page in pages:
+            page = db.pages.find_one({"_id": page})
+            text = page["fulltext"]
+            fulltext = fulltext + text
+        fulltext = fulltext.replace("\n", "")
+        fulltext = fulltext.replace("\r", "")
+        fulltext = fulltext.replace("\xa0", "")
+        lines = fulltext.split(".")
+        print(lines)
+        results = dandelion_ner(lines[0])
+        print("Results:\n")
+        print(results)
+        
+if __name__ == "__main__":
+    main()
+
