65f021a281f563c59eda22a955e07fc4acab8e64
marionkramer <marion.kramer@epfl.ch>
2017-11-30 14:37:26 +0100

    Add of functions to process articles
    It add the articles in the database, but not sure if it works for
    adding pulses

--- project_script.py
+++ project_script.py
@@ -11,32 +11,32 @@ def dandelion_ner(text, token):
  headers = {'text':text,'lang':'it','include':'types,lod,categories','epsilon':'0.3', 'token': token}
  r = requests.post(url,data=headers)
  if r.status_code == requests.codes.ok:
-     data = json.loads(r.text)
-     if "error" in data.keys():
-         return "API error. Status: "+str(data['status'])+" Code: "+data['code']+" Message: "+data['message']
-     results = list()
+	 data = json.loads(r.text)
+	 if "error" in data.keys():
+		 return "API error. Status: "+str(data['status'])+" Code: "+data['code']+" Message: "+data['message']
+	 results = list()
  # process Dandelion (Spazio dati) data
  # all entity types are dbpedia.org/ontology entities
-     for item in data['annotations']:
-         entity_type = ", ".join([w.split("/")[-1] for w in item['types']])
-         entity_label = item['label']
-         entity_spot = item['spot']
-         entity_category = ""
-         if "categories" in item.keys():
-             entity_category = ", ".join([w.split("/")[-1] for w in item['categories']])
-             dbpedia = ""
-             wikipedia = ""
-         if "lod" in item.keys():
-             dbpedia = item['lod']['dbpedia']
-             wikipedia = item['lod']['wikipedia']
-             results.append({"label": entity_label, "type": entity_type, "spot": entity_spot, "category": entity_category, "relevance": item['confidence'], "word": item['spot'], "offset": item['start'], "identifier": item['uri'], "title": item['title'], "dbpedia": dbpedia, "wikipedia": wikipedia})
+	 for item in data['annotations']:
+		 entity_type = ", ".join([w.split("/")[-1] for w in item['types']])
+		 entity_label = item['label']
+		 entity_spot = item['spot']
+		 entity_category = ""
+		 if "categories" in item.keys():
+			 entity_category = ", ".join([w.split("/")[-1] for w in item['categories']])
+			 dbpedia = ""
+			 wikipedia = ""
+		 if "lod" in item.keys():
+			 dbpedia = item['lod']['dbpedia']
+			 wikipedia = item['lod']['wikipedia']
+			 results.append({"label": entity_label, "type": entity_type, "spot": entity_spot, "category": entity_category, "relevance": item['confidence'], "word": item['spot'], "offset": item['start'], "identifier": item['uri'], "title": item['title'], "dbpedia": dbpedia, "wikipedia": wikipedia})
  #print "id "+urllib.unquote(item['uri']).encode('latin-1')
-     return results
+	 return results
  else:
   return "Web error: "+str(r.status_code)
 
 def utf8len(s):
-    return len(s.encode('utf-8'))
+	return len(s.encode('utf-8'))
 
 def clean_text(text):
 	text = text.replace("\n", "")
@@ -87,6 +87,31 @@ def write_pulse_type1(entity, title, author, page_number, pages, output_db, inpu
 	"wikipedia_resource": wikipedia_resource})
 	
 	return pulse_id, page_number
+
+def write_pulse_type1_articles(entity, title, author, journal_title, volume, page_number, pages, output_db, input_db):
+	entity_label = entity["label"]
+	entity_spot = entity["spot"]
+	wikipedia_resource = entity["wikipedia"]
+	
+	if page_number == -1:
+		page_number = scan_pages(input_db, entity, pages)
+
+	authors = author[0]
+	for i in range(1, len(author)):
+		authors = authors + " and " + author[i]
+
+	
+	pulse = entity_label + " (" + wikipedia_resource + ") " + "is present in article '" + title + "' by " + authors + " at page " + str(page_number) + " in the volume " + volume + "of journal " + journal_title + "."
+	
+	#actual writing of the pulse
+	pulse_id = output_db.pulses.insert({"type": 1, 
+	"pulse": pulse, 
+	"entity_name": entity_label, 
+	"reference": entity_spot,
+	"page_number": page_number, 
+	"wikipedia_resource": wikipedia_resource})
+	
+	return pulse_id, page_number
 	
 def write_pulse_type2(entity1, entity1_page_number, entity2, title, author, pages, output_db, input_db):
 	entity1_label = entity1["label"]
@@ -117,10 +142,42 @@ def write_pulse_type2(entity1, entity1_page_number, entity2, title, author, page
 	})
 	
 	return pulse_id, entity2_page_number
+
+def write_pulse_type2_articles(entity1, entity1_page_number, entity2, title, author, journal_title, volume, pages, output_db, input_db):
+	entity1_label = entity1["label"]
+	entity1_spot = entity1["spot"]
+	entity1_wikipedia_resource = entity1["wikipedia"]
 	
-def write_pulses(results, metadata, pages, output_db, input_db):
-	author = metadata["creator"]
-	title = metadata["title"]["surface"]
+	entity2_label = entity2["label"]
+	entity2_spot = entity2["spot"]
+	entity2_wikipedia_resource = entity2["wikipedia"]
+
+	authors = author[0]
+	for i in range(1, len(author)):
+		authors = authors + " and " + author[i]
+	
+	entity2_page_number = scan_pages(input_db, entity2, pages)
+	
+	page_difference = math.ceil(math.fabs(entity1_page_number- entity2_page_number))
+	
+	pulse = entity1_label + " (" + entity1_wikipedia_resource + ") " +  " and " + entity2_label + " (" + entity2_wikipedia_resource + ") " + " are " + str(page_difference) + " pages distant in the article " + title + " by " + authors + "present in volume " + volume + "of journal " + journal_title + "." 
+	
+	#actual writing of the pulse
+	pulse_id = output_db.pulses.insert({"type": 2, 
+	"pulse": pulse, 
+	"entity1_name": entity1_label, 
+	"entity1_page_number": entity1_page_number, 
+	"entity1_wikipedia_resource": entity1_wikipedia_resource,
+	"reference1": entity1_spot,
+	"entity2_name": entity2_label, 
+	"entity2_page_number": entity2_page_number, 
+	"entity2_wikipedia_resource": entity2_wikipedia_resource,
+	"reference2": entity2_spot,
+	})
+	
+	return pulse_id, entity2_page_number
+	
+def write_pulses(results, metadata, pages, output_db, input_db, type):
 	
 	pulses_id = list()
 	pulse_id1 = -1
@@ -128,18 +185,39 @@ def write_pulses(results, metadata, pages, output_db, input_db):
 	numb_entities = len(results)
 	page_number_entity_1 = -1
 	page_number_entity_2 = -1
+
+	if type == "book":
+		author = metadata["creator"]
+		title = metadata["title"]["surface"]
 	
-	for index, entity_1 in enumerate(results):
-		pulse_id1 = -1
-		if page_number_entity_2 != -1:
-			pulse_id1, page_number_entity_1 = write_pulse_type1(entity_1, title, author, page_number_entity_2, pages, output_db, input_db)
-		else:
-			pulse_id1, page_number_entity_1 = write_pulse_type1(entity_1, title, author, -1, pages, output_db, input_db)
-		if index < numb_entities-1:
-			entity_2 = results[index+1]
-			pulse_id2, page_number_entity_2  = write_pulse_type2(entity_1, page_number_entity_1, entity_2, title, author, pages, output_db, input_db)
-		pulses_id.append(pulse_id1)
-		pulses_id.append(pulse_id2)
+		for index, entity_1 in enumerate(results):
+			pulse_id1 = -1
+			if page_number_entity_2 != -1:
+				pulse_id1, page_number_entity_1 = write_pulse_type1(entity_1, title, author, page_number_entity_2, pages, output_db, input_db)
+			else:
+				pulse_id1, page_number_entity_1 = write_pulse_type1(entity_1, title, author, -1, pages, output_db, input_db)
+			if index < numb_entities-1:
+				entity_2 = results[index+1]
+				pulse_id2, page_number_entity_2  = write_pulse_type2(entity_1, page_number_entity_1, entity_2, title, author, pages, output_db, input_db)
+			pulses_id.append(pulse_id1)
+			pulses_id.append(pulse_id2)
+	else:
+		author = metadata["authors"]
+		title = metadata["title"]
+		journal_title = metadata["journal_short_title"]
+		volume = metadata["volume"]
+
+		for index, entity_1 in enumerate(results):
+			pulse_id1 = -1
+			if page_number_entity_2 != -1:
+				pulse_id1, page_number_entity_1 = write_pulse_type1_articles(entity_1, title, author, journal_title, volume, page_number_entity_2, pages, output_db, input_db)
+			else:
+				pulse_id1, page_number_entity_1 = write_pulse_type1_articles(entity_1, title, author, journal_title, volume, -1, pages, output_db, input_db)
+			if index < numb_entities-1:
+				entity_2 = results[index+1]
+				pulse_id2, page_number_entity_2  = write_pulse_type2_articles(entity_1, page_number_entity_1, entity_2, title, author, journal_title, volume, pages, output_db, input_db)
+			pulses_id.append(pulse_id1)
+			pulses_id.append(pulse_id2)
 	
 	return pulses_id
 	
@@ -158,6 +236,16 @@ def write_book(results, metadata, pulses_id, output_db):
 	"sbn_id": metadata["sbn_id"],
 	"pulses": pulses_id
 	 })
+	return True
+
+def write_articles(results, metadata, pulses_id, output_db):
+	output_db.articles.insert_one({"authors": metadata["authors"], 
+		"journal_bid": metadata["journal_bid"], 
+		"journal_short_title": metadata["journal_short_title"], 
+		"title": metadata["title"], 
+		"year": metadata["year"], 
+		"volume": metadata["volume"],
+		"pulses": pulses_id})
 	return True
 	
 def process_books(input_db, output_db, token_used):
@@ -183,7 +271,7 @@ def process_books(input_db, output_db, token_used):
 			results = dandelion_ner(text, token_used)
 			print("Results: " + str(results))
 			#keep processing
-			pulses_id = write_pulses(results, metadata, pages, output_db, input_db)
+			pulses_id = write_pulses(results, metadata, pages, output_db, input_db, "book")
 			write_book(results, metadata, pulses_id, output_db)
 		else:
 			lines = fulltext.split(".")
@@ -202,12 +290,77 @@ def process_books(input_db, output_db, token_used):
 				results = dandelion_ner(text, token_used)
 				print("Results: " + str(results))
 				#keep processing
-				pulses_id = write_pulses(results, metadata, pages, output_db, input_db)
+				pulses_id = write_pulses(results, metadata, pages, output_db, input_db, "book")
 				write_book(results, metadata, pulses_id, output_db)
 				
 				j = i
 				text = ""
-				
+
+def process_articles(input_db, output_db, token_used):
+	articles = input_db.bibliodb_articles.find({})
+   
+	for article in articles:
+		#get metadata of articles
+		metadata = {"authors": article["authors"], 
+		"journal_bid": article["journal_bid"], 
+		"journal_short_title": article["journal_short_title"], 
+		"title": article["title"], 
+		"year": article["year"], 
+		"volume": article["volume"]}
+		#get the id of the journal containing the article
+		journal_id = article["document_id"]
+		#get the place of the article in the journal
+		number = article["internal_id"][-2:]
+		if number[0] == ":":
+			number = number[1]
+		#-1 to have the right index as it starts from 0 for list
+		number = int(number) - 1
+		
+		journal = input_db.documents.find_one({"_id": journal_id})
+		ar = journal["articles"][number]
+		start_page = int(ar["start_page"])
+		end_page = int(ar["end_page"])
+
+		fulltext = ""
+		pages = []
+	   
+		for i in range(start_page, end_page + 1):
+			pages.append(journal["pages"][i])
+		
+		for page in pages:
+			page = input_db.pages.find_one({"_id": page})
+			text = page["fulltext"]
+			fulltext = fulltext + text
+
+		fulltext = clean_text(fulltext)
+		fulltext_length = len(fulltext)
+		print("fulltext length:" + str(len(fulltext)))
+
+		if fulltext_length < 1000000:
+			results = dandelion_ner(text, token_used)
+			print("Results: " + str(results))
+			pulses_id = write_pulses(results, metadata, pages, output_db, input_db, "journal")
+			write_articles(results, metadata, pulses_id, output_db)
+		else:
+			lines = fulltext.split(".")
+			nb_lines = len(lines)
+			text = ""
+			i = 0
+			j = 0
+
+			while j < nb_lines:
+				while i < nb_lines and utf8len(text) < 1000000:
+					print("length:" + str(utf8len(text)))
+					text = text + lines[i]
+					i += 1
+				print(text)
+				results = dandelion_ner(text, token_used)
+				print("Results: " + str(results))
+				pulses_id = write_pulses(results, metadata, pages, output_db, input_db, "journal")
+				write_articles(results, metadata, pulses_id, output_db)
+				j = i
+				text = ""
+   
 def main():
 	token_hakim = 'f3238f9b8e974df09b6814de9e9de532'
 	token_marion = 'ecd8d2b438484d92a593bf8274704cae'
@@ -215,9 +368,9 @@ def main():
 	
 	input_db, output_db = connect()
 	
-	process_books(input_db, output_db, token_used)
+	#process_books(input_db, output_db, token_used)
+	process_articles(input_db, output_db, token_marion)
 	
-    
-        
+		
 if __name__ == "__main__":
-    main()
+	main()
